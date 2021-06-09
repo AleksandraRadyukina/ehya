e6f51af0d4b7df91bcfe0b209e532816b45b6a76
AleksandraRadyukina <RadyukinaAleksandra@gmail.com>
2021-06-10 00:06:53 +0300

    create: connecting a slider in category sections and unreleased

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
-const swiper = new Swiper('.category__slider-container', {
+const swiper = new Swiper(".category__slider-container", {
   // Optional parameters
   loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
+  keyboard: {
+    enabled: true,
   },
-
   // Navigation arrows
   navigation: {
-    nextEl: '.category__button-next',
+    nextEl: ".category__button-next",
   },
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slideClass: "category-slide",
+});
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+// const unreleasedSlider = new Swiper(".unreleased__slider", {
+//   loop: true,
+//   keyboard: {
+//     enabled: true,
+//   },
+//   slidesPerVeiw: 5,
+//   spaceBetween: 30,
+//   slideClass: "unreleased__slide",
+// });
+
+const unreleasedSlider = new Swiper(".unreleased__slider", {
+  loop: true,
+  slidesPerView: 5,
+  spaceBetween: 30,
+  slideClass: "unreleased__slide",
+  navigation: {
+    nextEl: ".unreleased__button-next",
+    prevEl: ".unreleased__button-prev",
   },
 });
+
+
